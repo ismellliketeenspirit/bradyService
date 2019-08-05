d7ee77bd5242bdd9c9a85d5fc19f08ae97385460
Brady <brady.y.liu@gmail.com>
2019-08-05 00:14:44 -0700

    Cleaned up Seed

--- database/postgrsql/index.js
+++ database/postgrsql/index.js
@@ -1,12 +1,14 @@
 const pg = require('pg')
 // const client = new Client();
-const pool = pg.Pool({
-  user: 'brady',
-  host: 'localhost',
-  database: 'sdc-zon13',
-  password: '',
-  port: 3333,
-})
+// const pool = pg.Pool({
+//   user: 'bradyliu',
+//   host: 'localhost',
+//   database: 'sdc-zon13',
+//   password: '',
+//   port: 3333,
+// })
+
+const pool = new pg.Pool({ database: 'sdc-zon13' });
 
 pool.connect()
   .then(() => console.log('connected to postgreSQL'));
@@ -14,15 +16,15 @@ pool.connect()
   const createTable = () => {
     const createTable = 
     `CREATE TABLE product_info (
-       productID INTEGER AUTO INCREMENT,
+       productID INTEGER,
        seller VARCHAR (50) NOT NULL,
        productName VARCHAR (255) NOT NULL,
-       price integer NOT NULL,
+       price numeric NOT NULL,
        productDesc VARCHAR (255) NOT NULL,
        styles VARCHAR (255),
        options VARCHAR (255),
        bulletOne VARCHAR (255),
-       bulletTwo VARCHAR (255),
+       bulletTwo VARCHAR (255)
     )`;
     pool
       .query(createTable)
@@ -33,10 +35,9 @@ pool.connect()
       .catch((e) => {
         console.log(e);
         pool.end();
-
       })
   }
-
+  createTable();
   const insertRecords = (id, merchant, name, price, description, styles, options, bulletone, bullettwo) => {
     const query = `INSERT INTO product VALUES (${id}, ${merchant}, ${name}, ${price}, ${description}, ${styles}, ${options}, ${bulletone}, ${bullettwo} )`;
     pool
--- database/postgrsql/seed.js
+++ database/postgrsql/seed.js
@@ -1,48 +1,49 @@
 const faker = require('faker');
 const pgp = require('pg-promise')();
 
-const connection = {
-  user: 'brady',
-  host: 'localhost',
-  database: 'sdc-zon13',
-  password: '',
-  port: 3333,
-};
-const db = pgp(connection);
+// const connection = {
+//   user: 'bradyliu',
+//   host: 'localhost',
+//   database: 'sdc-zon13',
+//   password: '',
+//   port: 3333,
+// };
+const db = pgp({ database: 'sdc-zon13' });
 const startTimer = new Date().getTime();
 
 // create data
 const generateData = (amountOfData) => {
   const data = [];
   for (let i = 0; i < amountOfData; i++) {
-    data.push({ id: i, name: `${faker.name.firstName()} + ${faker.name.lastName()}`, 
-    productName: faker.commerce.productName(), 
-    price: faker.commerce.price(), 
-    productDesc: faker.lorem.sentence(),
-    styles: faker.commerce.color(),
-    options: faker.commerce.productMaterial(),
-    bulletOne: faker.lorem.sentence(),
-    bulletTwo: faker.commerce.sentence()
-   });
+    data.push({
+      productid: i,
+      seller: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      productname: faker.commerce.productName(),
+      price: faker.commerce.price(),
+      productdesc: faker.lorem.sentence(),
+      styles: faker.commerce.color(),
+      options: faker.commerce.productMaterial(),
+      bulletone: faker.lorem.sentence(),
+      bullettwo: faker.lorem.sentence()
+    });
   }
-  return pgp.helpers.insert(data, ['id', 'name'], 'product');
+  return pgp.helpers.insert(data, ['productid', 'seller', 'productname', 'price', 'productdesc', 'styles', 'options', 'bulletone', 'bullettwo'], 'product_info');
 };
 
-// load data
 db.tx(async (t) => {
   for (let i = 0; i < 1000; i++) {
     await t.none(generateData(10000));
-    console.log(`Batch ${i + 1} of 1000 Completed`);
+    console.log(`${i + 1} chunks loaded`);
   }
 })
   .then(() =>
-    console.log('[Complete]: 10,000,000 records inserted successfully'),
+    console.log('10m records packed together nicely'),
   )
   .catch((e) => console.log(e))
   .finally(() => {
     const endTimer = new Date().getTime();
     console.log(
-      `[Execution Time]: ${Math.round((endTimer - startTimer) / 1000, 2)} seconds`,
+      `Run Time: ${Math.round((endTimer - startTimer) / 1000, 2)} seconds`,
     );
     pgp.end();
   });
